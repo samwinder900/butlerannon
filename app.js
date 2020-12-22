edd613afa5503793d76ad096eb07b386135b096b
samwinder900 <samwinder900@gmail.com>
2020-12-22 20:10:17 +0000

    Form created

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 "use strict";
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'pug');
-
 app.set('views', './src/views');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.render('index');
+});
+
+app.post('/submit', (req,res) => {
+    console.log("Got body: ", req.body);
+    res.sendStatus(200);
 });
 
 module.exports = app;
